56b2b36c72054830c30dc94191bd8734606cc1d4
nwkami <715190184@qq.com>
2021-09-03 13:24:29 +0800

    新建editLabel.vue

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Layout>
+      编辑标签
+    </Layout>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class EditLabel extends Vue {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
