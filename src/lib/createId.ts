d6b71f8db10104673dd07eee28a539e661510101
nwkami <715190184@qq.com>
2021-09-03 17:52:05 +0800

    refactor: recordListModel.ts

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,8 +1,10 @@
 
 let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
-
+console.log(id);
 function createId() {
+  console.log(id);
   id++;
+  window.localStorage.setItem('_idMax', id.toString());
   return id;
 }
 
