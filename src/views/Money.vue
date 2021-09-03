c80d2944658c241c485758f5c55f1edaf056f08a
nwkami <715190184@qq.com>
2021-09-03 12:56:49 +0800

    数据迁移与升级

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template >
   <Layout class-prefix="layout">
-    {{record}}
+    {{recordList}}
 
     <NumberPad :value="record.amount" @update:value="onUpdateAmount" @submit="saveRecord"/>
     <!--<Types :value="record.type" @update:value="onUpdateType"/>-->
@@ -23,11 +23,24 @@ import Types from '@/components/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Notes from '@/components/Money/Notes.vue';
 
+const version = window.localStorage.getItem('version') || '0';
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+if (version === '0.0.1') {
+  //数据迁移 | 数据库升级
+  recordList.forEach(record => {
+    record.createdAt = new Date(2020, 0, 1);
+  });
+  //升级完成，保存数据
+  window.localStorage.setItem('recordList', JSON.stringify(recordList));
+}
+//升级版本
+window.localStorage.setItem('version', '0.0.2');
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createdAt?: Date;
 }
 
 @Component({
@@ -35,7 +48,7 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '彩票'];
-  recordList: Record[] = [];
+  recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
   record: Record = {
     tags: [],
     notes: '',
@@ -57,7 +70,8 @@ export default class Money extends Vue {
 
   saveRecord(){
     // 深拷贝：先变成字符串，再变成对象，这样就不是同一个内存地址了
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2 :Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
   }
