d5ac865cd19a99cbd2b4e2e6893deb2fe98f7478
nwkami <715190184@qq.com>
2021-09-04 15:35:56 +0800

    style: tabs.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -21,7 +21,7 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Types.vue';
+
 import Tags from '@/components/Money/Tags.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import recordTypeList from '@/constants/recordTypeList';
@@ -34,7 +34,7 @@ import Tabs from '@/components/Tabs.vue';
 
 
 @Component({
-  components: {FormItem, Tags,  Types, NumberPad,Tabs},
+  components: {FormItem, Tags,  NumberPad,Tabs},
 
 })
 export default class Money extends Vue {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -8,18 +8,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import Types from '@/components/Money/Types.vue';
+
 import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import intervalList from '@/constants/intervalList';
 @Component({
-  components: {Tabs, Types}
+  components: {Tabs}
 })
 export default class Statistics extends Vue{
   type = '-';
   interval = 'day';
   intervalList = intervalList;
-  recordTypeList = recordTypeList
+  recordTypeList = recordTypeList;
 
 }
 </script>
@@ -27,11 +27,18 @@ export default class Statistics extends Vue{
 <style lang="scss" scoped>
 ::v-deep .type-tabs-item {
   background: white;
+
   &.selected {
     background: #c4c4c4;
+
+    &::after {
+      display: none;
+    }
   }
-  &::after {
-    display: none;
+
+  ::v-deep .interval-tabs-item {
+
+    height: 48px;
   }
 }
 </style>
