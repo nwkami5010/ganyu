21825cccc83cb10caa7108fed4267b28a58b0f67
nwkami <715190184@qq.com>
2021-09-04 14:57:57 +0800

    删除computed函数，改从vuex获取get

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -18,17 +18,14 @@ import {Component} from 'vue-property-decorator';
 import {mixins} from 'vue-class-component';
 
 import TagHelper from '@/mixins/TagHelper';
-@Component({
-  computed:{
-    tagList(){
-      return this.$store.state.tagList;
-    }
-  }
-})
+@Component
 export default class Tags extends mixins(TagHelper){
   // readonly外部数据只读
 //tagList = store.fetchTags();
   selectedTags: string[] = [];
+  get tagList(){
+    return this.$store.state.tagList;
+  }
   created() {
     this.$store.commit('fetchTags');
   }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -33,21 +33,19 @@ import FormItem from '@/components/Money/FormItem.vue';
 
 @Component({
   components: {FormItem, Tags,  Types, NumberPad},
-  computed: {
-    recordList(){
-      return this.$store.state.recordList;
-    }
-  }
+
 })
 export default class Money extends Vue {
-
+  get recordList() {
+    return this.$store.state.recordList;
+  }
 
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
 
-  created(){
-    this.$store.commit('fetchRecords')
+  created() {
+    this.$store.commit('fetchRecords');
   }
 
   onUpdateNotes(value: string) {
